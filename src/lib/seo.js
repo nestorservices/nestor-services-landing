c046ab527839a9c345763d11cfa668778e521d2f
Shashank <smalviya22@gmail.com>
2026-04-09 10:38:09 +0530

    fix canonical seo

--- src/lib/seo.js
+++ src/lib/seo.js
@@ -1,11 +1,17 @@
 export function setCanonical(url) {
-  let canonical = document.querySelector('link[rel="canonical"]');
+  let link = document.querySelector("link[rel='canonical']");
 
-  if (!canonical) {
-    canonical = document.createElement("link");
-    canonical.setAttribute("rel", "canonical");
-    document.head.appendChild(canonical);
+  if (!link) {
+    link = document.createElement("link");
+    link.setAttribute("rel", "canonical");
+    document.head.appendChild(link);
   }
 
-  canonical.setAttribute("href", url);
+  link.setAttribute("href", url);
+
+  document
+    .querySelectorAll("link[rel='canonical']")
+    .forEach((canonicalLink) => {
+      if (canonicalLink !== link) canonicalLink.remove();
+    });
 }
